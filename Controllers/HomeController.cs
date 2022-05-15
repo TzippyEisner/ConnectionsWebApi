e7600b74d998cb6f95c9918d3edb71f4cdff7959
TzippyEisner <tzippy26@gmail.com>
2022-05-15 14:13:46 +0300

    Connect DB

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,22 +16,25 @@ namespace ConnectionsWebApi.Controllers
       return View();
     }
 
-    //[System.Web.Http.HttpGet]
-    //public string GetCustomer(int customerId)
-    //{
-    //  try
-    //  {
-    //    Customer customer;
-    //    using (var db = new Weekend2011Entities())
-    //    {
-    //      customer = db.Customer.FirstOrDefault(x => x.customerID == customerId);
-    //    }
-    //    return customer.customerName;
-    //  }
-    //  catch (Exception ex)
-    //  {
-    //    throw ex;
-    //  }
-    //}
+    //https://localhost:44362/home/GetUserNameByUnique?uniqueID=1B2AC001-D56B-451E-A794-0AFB02E03B0D
+    [System.Web.Http.HttpGet]
+    public string GetUserNameByUnique(string uniqueID)
+    {
+      try
+      {
+        users user = new users();
+        using (connectionsEntities db = new connectionsEntities())
+        {
+          user = db.users.FirstOrDefault(x => x.UniqeID == uniqueID);
+        }
+        if (user != null)
+          return user.FirstName + " " + user.LastName;
+        return string.Empty;
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
   }
 }
